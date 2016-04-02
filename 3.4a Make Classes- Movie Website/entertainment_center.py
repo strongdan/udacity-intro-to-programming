39fbb0e19039dcbde6eea80572b560674bac6d7e
Daniel Strong <danielstrong@mac.com>
2016-04-02 06:56:57 -0800

    work on trailer website

--- 3.4a Make Classes- Movie Website/entertainment_center.py
+++ 3.4a Make Classes- Movie Website/entertainment_center.py
@@ -15,9 +15,11 @@ import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikipedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-avatar = media.Movie()
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikipedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "http://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 movies = [toy_story, avatar]
+
 #fresh_tomatoes.open_movies_page(movies)
 
-print(toy_story.storyline)
+
+print (avatar.storyline)
