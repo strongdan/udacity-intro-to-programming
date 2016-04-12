195b1910da26b889e5ee3c49fbe77b8b57aecc77
Dan Strong <strongdan@users.noreply.github.com>
2016-04-12 15:53:25 -0800

    Update user_prompt.py

--- 2.8 Stage 2 - Work Session 5/user_prompt.py
+++ 2.8 Stage 2 - Work Session 5/user_prompt.py
@@ -6,6 +6,6 @@ adding ___2___ separated by commas between the parentheses. ___1___s by default 
 don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
 tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
 
-w1, w2, w3, w4 = input("Please type a word for each of the blanks above.\n" + sample + '\n\nWords: ').split()
+w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + sample + '\n\nWords: ').split()
 
 print(w1, w2, w3, w4)
