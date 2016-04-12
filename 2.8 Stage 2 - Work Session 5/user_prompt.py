f174395e02c45d205dbb73b4565a3ed4673519f5
Dan Strong <strongdan@users.noreply.github.com>
2016-04-12 15:48:53 -0800

    Create user_prompt.py

---
+++ 2.8 Stage 2 - Work Session 5/user_prompt.py
@@ -0,0 +1,11 @@
+# prompt a user with a sentence containing several blanks
+# The user should then be asked to fill in each blank appropriately to complete the sentence
+
+sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+w1, w2, w3, w4 = input("Please fill in the blanks:\n" + sample + '\n\nWords: ').split()
+
+print(w1, w2, w3, w4)
