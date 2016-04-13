e920ebe8dbc180d397cecd15621d92a4d70f17a3
Dan Strong <strongdan@users.noreply.github.com>
2016-04-12 21:10:58 -0800

    Update user_prompt.py

--- 2.8 Stage 2 - Work Session 5/user_prompt.py
+++ 2.8 Stage 2 - Work Session 5/user_prompt.py
@@ -1,13 +1,14 @@
+blanks = ["___1___", "___2___", "___3___", "___4___"]
+
+# - Level 0 Text - Easy
 sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
 adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
 don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
 tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-# ANSWERS:
-# ___1___ => function
-# ___4___ => 
-# ___3___ =>
-# ___4___ =>
 
+sample_answers = ['function', 'variables', 'outputs', 'lists']
+
+# Level 1 Text - Moderate
 text1 = '''If you do need to iterate over a sequence of numbers, the built-in function ___1___
 comes in handy. It generates arithmetic progressions. The given end point is never part of the
 generated sequence; ___1___(10) generates 10 values, the legal indices for items of a sequence
@@ -19,12 +20,10 @@ a list, but in fact it isn’t. It is an object which returns the successive ite
 sequence when you iterate over it, but it doesn’t really make the list, thus saving space. We say
 such an object is ___4___, that is, suitable as a target for functions and constructs that expect
 something from which they can obtain successive items until the supply is exhausted.'''
-# ANSWERS:
-# ___1___ => range
-# ___4___ => len
-# ___3___ => enumerate
-# ___4___ => iterable
 
+lvl1_answers = ['range', 'len', 'enumerate', 'iterable']
+
+# Level 2 Text - Hard
 text2 = '''The ___1___ statement breaks out of the smallest enclosing for or while loop.
 Loop statements may have an else clause; it is executed when the loop terminates through
 exhaustion of the list (with for) or when the condition becomes false (with while), but
@@ -38,12 +37,10 @@ allowing you to keep thinking at a more abstract level. If the ___4___ statement
 for loop, the ___4___ statement is executed when the loop has exhausted iterating the list.
 If the ___4___ statement is used with a while loop, the ___4___ statement is executed when
 the condition becomes false.'''
-# ANSWERS:
-# ___1___ => break
-# ___4___ => pass
-# ___3___ => continue
-# ___4___ => else
 
+lvl2_answers = ['break', 'pass', 'continue', 'else']
+
+# Level 3 Text - Harder
 text3 = '''Small anonymous functions can be created with the ___1___ keyword. This function
 returns the sum of its two arguments: ___1___ a, b: a+b. ___1___ functions can be used wherever
 function objects are required. They are syntactically restricted to a single expression.
@@ -56,25 +53,24 @@ create a subsequence of those elements that satisfy a certain condition. A tuple
 of immutable Python objects. ___4___s are sequences, just like lists. The differences between
 ___4___s and lists are, the ___4___s cannot be changed unlike lists and ___1___s use parentheses,
 whereas lists use square brackets.'''
-# ANSWERS:
-# ___1___ => lambda
-# ___4___ => bitwise
-# ___3___ => comprehension
-# ___4___ => tuple
 
+lvl3_answers = ['lambda', 'bitwise', 'comprehension', 'tuple']
 
-# user first selects a level
-user_in = int(input("Please select a level from 0 to 3:  ))
+def user_lvl():
+    # user first selects a level
+    user_in = int(input("Please select a level from 0 to 3:  ))
 
-# prompt a user with a sentence containing several blanks
-# The user should then be asked to fill in each blank appropriately to complete the sentence                   
-if user_in == 0
-    w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + sample + '\n\nWords: ').split() 
-elif user_in == 1
-    w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + text1 + '\n\nWords: ').split() 
-elif user_in == 2
-    w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + text2 + '\n\nWords: ').split()
-else:
-    w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + text3 + '\n\nWords: ').split() 
-
+    # prompt a user with a sentence containing several blanks
+    # The user should then be asked to fill in each blank appropriately to complete the sentence                   
+    if user_in == 0
+        w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + sample + '\n\nWords: ').split() 
+    elif user_in == 1
+        w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + text1 + '\n\nWords: ').split() 
+    elif user_in == 2
+        w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + text2 + '\n\nWords: ').split()
+    elif:
+        w1, w2, w3, w4 = input("Please type a word for each of the blanks above, separated by spaces.\n" + text3 + '\n\nWords: ').split() 
+    else:
+        break
+                    
 print(w1, w2, w3, w4)
